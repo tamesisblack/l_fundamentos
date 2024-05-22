5336c3a367e6662541a3a6a3e1867d7381483d55
tamesisblade <53984711+tamesisblade@users.noreply.github.com>
2024-05-21 22:45:17 -0500

    components

--- resources/views/layouts/_partials/menu.blade.php
+++ resources/views/layouts/_partials/menu.blade.php
@@ -4,9 +4,6 @@
         <li><a href="{{ route('about') }}">Acerca</a></li>
         <li><a href="{{ route('services') }}">Servicios</a></li>
         <li><a href="{{ route('contact') }}">Contacto</a></li>
-        {{-- <li><a href="/">Inicio</a></li>
-        <li><a href="/about">Acerca</a></li>
-        <li><a href="/services">Servicios</a></li>
-        <li><a href="/contact">Contacto</a></li> --}}
+        
     </ul>
 </nav>
--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -2,4 +2,28 @@
 @section('title', 'Services')
 @section('content')
     <h1>Services</h1>
+    @component('_components.card')
+        @slot('title')
+            Service 1
+        @endslot
+        @slot('content')
+            This is the content for service 1.
+        @endslot
+    @endcomponent
+    @component('_components.card')
+        @slot('title')
+            Service 2
+        @endslot
+        @slot('content')
+            This is the content for service 2.
+        @endslot
+    @endcomponent
+    @component('_components.card')
+        @slot('title')
+            Service 3
+        @endslot
+        @slot('content')
+            This is the content for service 3.
+        @endslot
+    @endcomponent
 @endsection
